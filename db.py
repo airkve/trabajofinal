b98bd27520ca301df21d3f2711d33a4f5e87c07d
Richard Jimenez <ricjim@gmail.com>
2019-10-10 17:13:05 -0300

    Update db.py

--- db.py
+++ db.py
@@ -4,14 +4,14 @@ from mysql.connector import Error
 from mysql.connector import errorcode
 
 class Database():
-  """ Modulo para consultar, modificar y conectarse a la base de datos. """
+    """ Modulo para consultar, modificar y conectarse a la base de datos. """
   
-  def __init__(self):
-      self.conexion = mysql.connector.connect(**dbconf)
-      self.cursor = self.conexion.cursor()
-    
-  def consulta_db(self, consulta):
-      pass
-    
-  def modificar_db(self, data):
-      pass
+    def __init__(self):
+        self.conexion = mysql.connector.connect(**dbconf)
+        self.cursor = self.conexion.cursor()
+        
+    def consulta_db(self, consulta):
+        pass
+        
+    def modificar_db(self, data):
+        pass
